7b5ff0038ae646efee7d77bf70f6c5ffeb559c78
emonovaev <lena.jemonova@gmail.com>
2024-01-28 14:42:17 +0300

    fix

--- src/test/java/Config/BrowserstackConfig.java
+++ src/test/java/Config/BrowserstackConfig.java
@@ -14,7 +14,6 @@ public interface BrowserstackConfig extends Config {
     String getKey();
 
     @Key("app")
-  //  @DefaultValue("//app=bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c")
     String getApp();
 
     @Key("device")
