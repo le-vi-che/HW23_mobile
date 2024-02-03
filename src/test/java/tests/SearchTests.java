c7a150baa9447df4aa116fc64936500f030812dd
emonovaev <lena.jemonova@gmail.com>
2024-02-03 16:20:42 +0300

    fix

--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -1,5 +1,7 @@
 package tests;
 
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
@@ -10,10 +12,13 @@ import static io.appium.java_client.AppiumBy.accessibilityId;
 import static io.appium.java_client.AppiumBy.id;
 import static io.qameta.allure.Allure.step;
 
+@Owner("emonovaev")
+
 public class SearchTests extends TestBase {
 
     @Test
-    void successfulSearchTest() {
+    @DisplayName("Проверка обнаружения результатов поиска")
+    void checkResultTest() {
         step("Отправляем запрос в википедии", () -> {
             $(accessibilityId("Search Wikipedia")).click();
             $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
@@ -22,6 +27,17 @@ public class SearchTests extends TestBase {
         step("Проверяем, что обнаружены результаты поиска", () ->
                 $$(id("org.wikipedia.alpha:id/page_list_item_title"))
                         .shouldHave(sizeGreaterThan(0)));
+    }
+
+
+
+    @Test
+    @DisplayName("Открытие первой найденной ссылки")
+    void successfulSearchTest() {
+        step("Отправляем запрос в википедии", () -> {
+            $(accessibilityId("Search Wikipedia")).click();
+            $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
+        });
 
         step("Нажимаем на первую найденную ссылку", () ->
                 $$(id("org.wikipedia.alpha:id/page_list_item_title")).first().click());
